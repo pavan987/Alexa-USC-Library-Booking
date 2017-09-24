37fd713000e964e188db791027caa710d5515e10
pavan987 <gondhi@usc.edu>
2017-09-24 13:57:13 -0700

    Final Code

--- index.js
+++ index.js
@@ -16,8 +16,47 @@ var handlers = {
         this.emit(':responseReady');
     },
 
+    'BookRoomIntent': function() {
+
+      // if (this.event.request.dialogState == "STARTED" || this.event.request.dialogState == "IN_PROGRESS"){
+      //             this.context.succeed({
+      //                 "response": {
+      //                     "directives": [
+      //                         {
+      //                             "type": "Dialog.Delegate"
+      //                         }
+      //                     ],
+      //                     "shouldEndSession": false
+      //                 },
+      //                 "sessionAttributes": {}
+      //             });
+      //         }
+      // else {
+      //   var say = ''
+      //   var date = this.event.request.intent.slots.date.value;
+      //   var time = this.event.request.intent.slots.time.value;
+      //   var room = this.event.request.intent.slots.room.value;
+      //   var endtime = this.event.request.intent.slots.endtime.value;
+
+
+
+        say = "Your Booking is Done. please check your email and confirm the booking. Have a Nice day!"
+
+
+
+        //say the results
+        this.response.speak(say);
+        this.emit(":responseReady");
+
+
+    //  }
+
+
+    },
+
     'FindRoomIntent': function () {
         // call the Fact Service
+        console.log("entered FindRoomIntent")
         if (this.event.request.dialogState == "STARTED" || this.event.request.dialogState == "IN_PROGRESS"){
                     this.context.succeed({
                         "response": {
@@ -34,32 +73,88 @@ var handlers = {
         else {
           var date = this.event.request.intent.slots.date.value;
           var time = this.event.request.intent.slots.time.value;
-          console.log("available date is "+date+" and time is "+time);
+          var end = this.event.request.intent.slots.endtime.value;
+          // console.log("end object is "+end)
+          //endtime = "";
+          // if (end != undefined){
+          //   endtime=end.value;
+          // }
+          if(end == undefined)
+          {
+            end = ""
+          }
+          console.log("available date is "+date+" and time is "+time+" end time is "+end);
 
-            var libraries = ['4490', '14133', '206']
+            var libraries = ['4490', '14133', '206','272' ]
+            var libnames = {'East Asian':'Doheny Library',  '3rd Floor':'Leavy library','2nd Floor':'Leavy library'}
+            var count=0;
             // "2017-09-27", "206", "14:00"
             for(var i=0; i<libraries.length; i++){
-                httpLibPost(date, libraries[i], time, Result => {
-                        console.log(Result);
+                httpLibPost(date, libraries[i], time, end, Result  => {
 
                         var say = '';
-                        r = JSON.parse(Result)
-                        if (r.status === true) {
-                            say = 'You are lucky! room '+r.response.room+' in leavey library is available from '+r.response.start
-                            +' to '+r.response.end;
-                            this.response.speak(say);
-                            this.emit(':responseReady');
-
-                        }
-                        else {
-                            if(i == libraries.length -1) {
-                                say = "Sorry! there is no room available during that time";
-                                this.response.speak(say);
-                                this.emit(':responseReady');
+                        console.log(Result)
+                        try {
+                          r = JSON.parse(Result)
+                          console.log(r)
+                          count = count+1;
+                          if (r.status === true) {
+                            var library = "Leavy library"
+                            for(var key in libnames) {
+                              if (r.slot1.room.indexOf(key) !== -1){
+                                library = libnames[key]
+                              }
                             }
-                        }
+                            console.log("entered status is true")
 
-                });
+                            say = 'You are lucky! in '+library+', room '+r.slot1.room+' is available from '+r.slot1.start
+                            +' to '+r.slot1.end; //What about Booking the room?
+
+                              //this.event.request.intent.slots.room.value=r.response.room;
+                              if(r.slot2.start != undefined) {
+                                  if(r.slot2.room == r.slot1.room) {
+                                    if(r.slot1.start < r.slot2.start ){
+                                    say = 'You are lucky! in '+library+', room '+r.slot1.room+' is available from '+r.slot1.start
+                                    +' to '+r.slot2.end; //What about Booking the room?
+                                    console.log("rooms are equal")
+                                  } else {
+                                    say = 'You are lucky! in '+library+', room '+r.slot1.room+' is available from '+r.slot2.start
+                                    +' to '+r.slot1.end; //What about Booking the room?
+                                    console.log("rooms are equal but slot 1 is greater")
+
+                                  }
+
+                                } else {
+                                  if(r.slot1.start < r.slot2.start ){
+                                  say = 'You are lucky! in '+library+', room '+r.slot1.room+' is available from '+r.slot1.start
+                                  +' to '+r.slot1.end+' and room '+r.slot2.room+' is available from '+r.slot2.start+' to '+r.slot2.end; //What about Booking the room?
+                                  console.log("rooms are not equal,")
+                                } else {
+                                  say = 'You are lucky! in '+library+', room '+r.slot2.room+' is available from '+r.slot2.start
+                                  +' to '+r.slot2.end+' and room '+r.slot1.room+' is available from '+r.slot1.start+' to '+r.slot1.end; //What about Booking the room?
+                                  console.log("rooms are not equal, but slot 1 is greater")
+                                }
+                              }
+                              }
+                              say += " Please say Book a Room or Cancel";
+                              console.log("saying "+say)
+                              this.response.speak(say).listen('Please say Book a Room or Cancel'); //.listen("Say Book a room or Cancel");
+                              this.emit(':responseReady');
+
+                          }
+                          else {
+                              if(count == libraries.length) {
+                                  say = "Sorry! there is no room available during that time. Say find a room again or cancel";
+                                  this.response.speak(say).listen(say);
+                                  this.emit(':responseReady');
+                              }
+                          }
+                      }catch (e) {
+                          say = "Sorry! There is a problem. Please try again";
+                          this.response.speak(say).listen(say);//.listen("Do you want to search other timing?");
+                          this.emit(':responseReady');
+                      }
+                  });
             }
 
        }
@@ -89,15 +184,15 @@ function mycallback(result){
 }
 var http = require('http');
 
-function httpScrapePost(Data, Time, callback) {
+function httpScrapePost(Data, Time, endTime, callback) {
 
     //var post_data = '{"html": "abc", "time":"14:00"}';
-    var post_data = {"html" : Data,"time" : Time}
+    var post_data = {"html" : Data,"start_time" : Time, "end_time": endTime}
     var data = JSON.stringify(post_data)
     var options = {
         host: 'alexa-skill-pentyala.c9users.io',
         port: 80,
-        path: '/',
+        path: '/test',
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -128,7 +223,7 @@ function httpScrapePost(Data, Time, callback) {
 }
 
 
-function httpLibPost(date, gid, time, callback) {
+function httpLibPost(date, gid, time, endtime, callback) {
 
     var post_data = {};
     var options = {
@@ -154,7 +249,7 @@ function httpLibPost(date, gid, time, callback) {
 
         res.on('end', () => {
             var abc = {}
-            httpScrapePost(returnData, time, function(ScrapeResult) {
+            httpScrapePost(returnData, time, endtime, function(ScrapeResult) {
               callback(ScrapeResult)
             });
 
@@ -165,6 +260,46 @@ function httpLibPost(date, gid, time, callback) {
     req.write(JSON.stringify(post_data));
     req.end();
 }
+
+// function httpScrapePost(Data, Time, callback) {
+//
+//     //var post_data = '{"html": "abc", "time":"14:00"}';
+//     var post_data = {"html" : Data,"time" : Time}
+//     var data = JSON.stringify(post_data)
+//     var options = {
+//         host: 'alexa-skill-pentyala.c9users.io',
+//         port: 80,
+//         path: '/',
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Content-Length': Buffer.byteLength(data),
+//           // 'Accept':'*/*',
+//           // 'Accept-Encoding': 'gzip, deflate',
+//           // 'Origin':"abcd"
+//         }
+//     };
+//
+//     var req = http.request(options, res => {
+//         res.setEncoding('utf8');
+//         var returnData = "";
+//
+//         res.on('data', chunk => {
+//             returnData = returnData + chunk;
+//         });
+//
+//         res.on('end', () => {
+//             callback(returnData);  // this will execute whatever function the caller defined, with one argument
+//
+//         });
+//
+//     });
+//     console.log(post_data)
+//     req.write(data);
+//     req.end();
+// }
+//
+
 // httpPost("2017-09-27","206",mycallback);
 // httpLibPost("2017-09-27", "206", "14:00", Result => {
 //         console.log(Result)
